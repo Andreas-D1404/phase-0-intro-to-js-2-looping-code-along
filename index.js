a8025480d75e0cdac94e27325bc4453f9ae583cd
Andrew <andrew.dimou10@gmail.com>
2024-03-13 15:26:27 +1100

    Initialize Git

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names) {
+  let thankYouMessages = [];
+  for (let name of names) {
+    thankYouMessages.push(
+      `Thank you, ${name}, for the wonderful surprise gift!`
+    );
+  }
+  return thankYouMessages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
